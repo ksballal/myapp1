cc24f70722ec7ff45137193be8236fece582a5ea
ksballal <suraj.ballal@gmail.com>
2017-10-10 11:18:48 +0530

    adding files for first time.
    added files for first time.

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		System.out.println( "New branch myapp2 is added!" );
+		System.out.println( "New branch myapp3 is added!" );
+		System.out.println( "New branch myapp4 is added!" );
+    }
+}
