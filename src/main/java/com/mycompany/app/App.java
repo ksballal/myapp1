bff47085441b8ff8149e4cdc3912cb79951b66d1
Suraj Ballal Katapady <suraj.katapady@zafin.com>
2017-10-10 10:52:50 +0530

    added new print for myapp4

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
 		System.out.println( "New branch myapp2 is added!" );
 		System.out.println( "New branch myapp3 is added!" );
+		System.out.println( "New branch myapp4 is added!" );
     }
 }
