5780b97bf7023a2ca31d95db9925b5b8604c1a3c
Suraj Ballal Katapady <suraj.katapady@zafin.com>
2017-10-10 11:51:54 +0530

    updated as per review comments

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //deleted this line added another line
+		
+		System.out.println( "myApp8" );
     }
 }
